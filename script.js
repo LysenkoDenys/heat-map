fd9acee3a7896822f9b35d7883343b7e13e1136b
Lysenko <lysenkoden@gmail.com>
2025-07-21 09:37:00 +0200

    4/10

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 const dataUrl =
-  'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
+  'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json';
 
-const svg = d3.select('svg');
 const w = 800;
 const h = 500;
-svg.attr('width', w).attr('height', h);
 const padding = 60;
+const baseTemp = 8.66;
 
 d3.select('body')
   .append('h1')
   .attr('id', 'title')
-  .append('h3')
-  .attr('id', 'description')
-  .append('text')
   .text('Monthly Global Land-Surface Temperature');
 
 d3.select('body')
   .append('h3')
   .attr('id', 'description')
-  .append('text')
-  .text('1753 - 2015: base temperature 8.66℃');
+  .text(`1753 - 2015: base temperature ${baseTemp} ℃`);
 
-d3.select('body').append('div').attr('class', 'container').append('svg');
+const svg = d3
+  .select('body')
+  .append('div')
+  .attr('class', 'container')
+  .append('svg')
+  .attr('width', w)
+  .attr('height', h);
 
+//=========================================================
 // const tooltip = d3
 //   .select('#tooltip')
 //   .style('opacity', 0)
@@ -33,14 +35,14 @@ const fetchTemperatureData = async () => {
     const response = await fetch(dataUrl);
     if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
     const dataset = await response.json();
-    const dates = dataset.map((d) => new Date(d['Year'], 0));
-    const parseTime = d3.timeParse('%M:%S');
-    const times = dataset.map((d) => parseTime(d['Time']));
-    const formatTime = d3.timeFormat('%M:%S');
+    const data = dataset.monthlyVariance;
+    const years = data.map((d) => new Date(d['year'], 0));
 
-    console.log(times); //
-    const minYear = d3.min(dates);
-    const maxYear = d3.max(dates);
+    // console.log(dataset.monthlyVariance[0]['year']); //
+    // console.log(years); //
+
+    const minYear = d3.min(years);
+    const maxYear = d3.max(years);
 
     const extendedMinYear = new Date(minYear);
     extendedMinYear.setFullYear(extendedMinYear.getFullYear() - 1);
@@ -48,62 +50,59 @@ const fetchTemperatureData = async () => {
     const extendedMaxYear = new Date(maxYear);
     extendedMaxYear.setFullYear(extendedMaxYear.getFullYear() + 1);
 
-    const extendedMinTime = new Date(d3.min(times));
-    extendedMinTime.setMinutes(extendedMinTime.getMinutes() - 1);
-
-    const extendedMaxTime = new Date(d3.max(times));
-    extendedMaxTime.setMinutes(extendedMaxTime.getMinutes() + 1);
-
     const xScale = d3
       .scaleTime()
       .domain([extendedMinYear, extendedMaxYear])
       .range([padding, w - padding]);
 
     const yScale = d3
-      .scaleTime()
-      .domain([d3.max(times), d3.min(times)])
-      .range([h - padding, padding]);
+      .scaleBand()
+      .domain(d3.range(1, 13)) // months 1–12
+      .range([padding, h - padding]);
 
     svg
-      .selectAll('circle')
-      .data(dataset)
+      .selectAll('rect')
+      .data(data)
       .enter()
-      .append('circle')
-      .attr('cx', (d) => xScale(new Date(d['Year'], 0)))
-      .attr('cy', (d) => yScale(parseTime(d['Time'])))
-      .attr('r', (d) => 5)
-      .attr('fill', (d) => (d['Doping'] ? 'orange' : 'green'))
-      .attr('class', 'dot')
-      .attr('data-xvalue', (d) => new Date(d['Year'], 0))
-      .attr('data-yvalue', (d) => parseTime(d['Time']))
-      // ========================================================
-      .on('mouseover', function (event, d) {
-        tooltip
-          .style('opacity', 1)
-          .style('display', 'block')
-          .attr('data-year', new Date(d.Year, 0))
-          .html(
-            `${d.Name}: ${d.Nationality}<br>Year: ${d.Year}, Time: ${d.Time}`
-          )
-          .style('left', parseInt(xScale(new Date(d['Year'], 0))) + 10 + 'px')
-          .style(
-            'top',
-            parseInt(yScale(parseTime(d['Time']))) + padding / 2 + 'px'
-          );
+      .append('rect')
+      .attr('x', (d) => xScale(new Date(d['year'], 0)))
+      .attr('y', (d) => yScale(d.month))
+      .attr('fill', 'green')
+      .attr(
+        'width',
+        (w - 2 * padding) / (maxYear.getFullYear() - minYear.getFullYear())
+      )
+      .attr('height', yScale.bandwidth())
+      .attr('data-year', (d) => d.year)
+      .attr('data-month', (d) => d.month - 1);
+    // ========================================================
+    // .on('mouseover', function (event, d) {
+    //   tooltip
+    //     .style('opacity', 1)
+    //     .style('display', 'block')
+    //     .attr('data-year', new Date(d.Year, 0))
+    //     .html(
+    //       `${d.Name}: ${d.Nationality}<br>Year: ${d.Year}, Time: ${d.Time}`
+    //     )
+    //     .style('left', parseInt(xScale(new Date(d['Year'], 0))) + 10 + 'px')
+    //     .style(
+    //       'top',
+    //       parseInt(yScale(parseTime(d['Time']))) + padding / 2 + 'px'
+    //     );
 
-        d3.select(this)
-          .attr('fill', 'red')
-          .transition()
-          .duration(200)
-          .attr('r', 7);
-      })
+    //   d3.select(this)
+    //     .attr('fill', 'red')
+    //     .transition()
+    //     .duration(200)
+    //     .attr('r', 7);
+    // })
 
-      .on('mouseout', function () {
-        tooltip.style('opacity', 0);
-        d3.select(this)
-          .attr('fill', (d) => (d.Doping ? 'orange' : 'green'))
-          .attr('r', 5);
-      });
+    // .on('mouseout', function () {
+    //   tooltip.style('opacity', 0);
+    //   d3.select(this)
+    //     .attr('fill', (d) => (d.Doping ? 'orange' : 'green'))
+    //     .attr('r', 5);
+    // });
     //===========================================================
     drawLegend(svg, w);
 
@@ -111,12 +110,14 @@ const fetchTemperatureData = async () => {
     const yAxis = d3.axisLeft(yScale).tickFormat(formatTime);
 
     svg
-      .select('#x-axis')
+      .append('g')
+      .attr('id', 'x-axis')
       .attr('transform', `translate(0,${h - padding})`)
       .call(xAxis);
 
     svg
-      .select('#y-axis')
+      .append('g')
+      .attr('id', 'y-axis')
       .attr('transform', `translate(${padding},0)`)
       .call(yAxis);
   } catch (error) {
